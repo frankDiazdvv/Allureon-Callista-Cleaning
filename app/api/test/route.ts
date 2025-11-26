da2f57bff731bc4dcedde5e0b56b1c2639d55550
Francisco <fmdiaz1802@gmail.com>
2025-11-26 10:23:15 -0500

    New deployment for test | No db

--- app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from 'next/server';
-// import { sql } from '@/lib/db';
-
-// export async function GET() {
-//   const result = await sql`SELECT 'Connected to Neon!' AS message`;
-//   return NextResponse.json({ message: result[0].message });
-// }
