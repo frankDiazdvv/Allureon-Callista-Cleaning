451d41819e70f064b53037bba0442b3348524648
Francisco <fmdiaz1802@gmail.com>
2025-11-26 10:21:43 -0500

    Commented out db code for now for deployment

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
-import { sql } from '@/lib/db';
+// import { NextResponse } from 'next/server';
+// import { sql } from '@/lib/db';
 
-export async function GET() {
-  const result = await sql`SELECT 'Connected to Neon!' AS message`;
-  return NextResponse.json({ message: result[0].message });
-}
+// export async function GET() {
+//   const result = await sql`SELECT 'Connected to Neon!' AS message`;
+//   return NextResponse.json({ message: result[0].message });
+// }
